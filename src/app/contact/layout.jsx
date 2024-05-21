0499a40c7a7f641f55e8de608d22f2fcea4d6294
nolimits20 <oladeleakomolede@yahoo.com>
2024-05-21 23:16:55 +0100

    fix: apos error

--- src/app/contact/layout.jsx
+++ src/app/contact/layout.jsx
@@ -1,41 +1,43 @@
-import React from 'react'
+import React from 'react';
 
-const layout = () => {
+const Layout = () => {
   return (
-    <div className='sm:grid-cols-2'>
-      <div className='px-8'>
-        <h1 className='pt-20 text-2xl sm:text-5xl lg:text-5xl md:text-3xl  text-black font-bold'>Contact Us</h1>
-        <p className='text-black text-sm pt-4'>Have a question, comment, or just want to say hello? We're here for you! <br /> Feel free to reach out to our friendly team for expert assistance, personalized <br /> recommendations, or any inquiries you may have</p>
+    <div className="px-8">
+      <div className="pt-20">
+        <h1 className="text-2xl sm:text-5xl md:text-3xl text-black font-bold">Contact Us</h1>
+        <p className="text-black text-sm pt-4">
+          Have a question, comment, or just want to say hello? We&apos;re here for you! <br />
+          Feel free to reach out to our friendly team for expert assistance, personalized <br />
+          recommendations, or any inquiries you may have.
+        </p>
       </div>
-      <div>
+      <div className="mt-8">
         <video
           src="/thank-you.mp4"
-          className=' px-8 mt-12 sm:h-[800px] w-full h-full object-cover'
+          className="w-full h-full sm:h-[800px] object-cover"
           autoPlay
           muted
           loop
         ></video>
       </div>
-      <div className='sm:grid grid-cols-2'>
-        <div className='px-8  w-full h-[500px]  mt-8'>
-          <h2 className='text-black text-lg sm:text-3xl pt-8'>Get in Touch</h2>
+      <div className="sm:grid sm:grid-cols-2 gap-8 mt-8">
+        <div className="w-full h-[500px]">
+          <h2 className="text-black text-lg sm:text-3xl pt-8">Get in Touch</h2>
           <div>
-          <input type="text" id='name' placeholder='FULL NAME'  className='border-b-black  border-b-2 w-full mt-10 p-2 bg-white outline-none'/>
-          <input type="email" id='email' placeholder='EMAIL'  className='border-b-black  border-b-2 w-full mt-10 p-2 bg-white outline-none'/>
-          <textarea name="message" id="message" placeholder='MESSAGE 'className='w-full mt-8 bg-white outline-none border-b-black border-b-2 p-2'></textarea>
+            <input type="text" id="name" placeholder="FULL NAME" className="border-b-2 border-black w-full mt-10 p-2 bg-white outline-none" />
+            <input type="email" id="email" placeholder="EMAIL" className="border-b-2 border-black w-full mt-10 p-2 bg-white outline-none" />
+            <textarea name="message" id="message" placeholder="MESSAGE" className="border-b-2 border-black w-full mt-8 p-2 bg-white outline-none"></textarea>
+          </div>
+          <div className="bg-black w-full text-center text-white mt-10 h-[50px] py-3">
+            <button>SEND A MESSAGE</button>
+          </div>
         </div>
-        <div className='bg-black w-full  text-center text-white mt-10 h-[50px] py-3'>
-          <button>SEND A MESSAGE</button>
-        </div>
-        </div>
-        <div className='px-8 w-full sm:w-[50%] h-[500px]  mt-8'>
-        <h2 className='text-black text-lg sm:text-3xl pt-8'>Lets Connect</h2>
+        <div className="w-full h-[500px]">
+          <h2 className="text-black text-lg sm:text-3xl pt-8">Let&apos;s Connect</h2>
         </div>
       </div>
-      
     </div>
-    
-  )
-}
+  );
+};
 
-export default layout
+export default Layout;
