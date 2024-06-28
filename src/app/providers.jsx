0f773915b2548a7f073341fe8afdc1ddb9b6794b
nolimits20 <oladeleakomolede@yahoo.com>
2024-06-28 09:59:06 +0100

    fix: local storage

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -1,25 +1,35 @@
-'use client'
+'use client';
 import React, { createContext, useState, useEffect } from 'react';
 
 const SideBarContext = createContext();
 
 const SidebarProvider = ({ children }) => {
   const [val, setVal] = useState(() => {
-    const savedVal = localStorage.getItem('get item');
-    return savedVal !== null ? parseInt(savedVal, 10) : 0;
+    if (typeof window !== 'undefined') {
+      const savedVal = localStorage.getItem('get item');
+      return savedVal !== null ? parseInt(savedVal, 10) : 0;
+    }
+    return 0;
   });
 
   const [cartItems, setCartItems] = useState(() => {
-    const savedCartItems = localStorage.getItem('cart items');
-    return savedCartItems !== null ? JSON.parse(savedCartItems) : [];
+    if (typeof window !== 'undefined') {
+      const savedCartItems = localStorage.getItem('cart items');
+      return savedCartItems !== null ? JSON.parse(savedCartItems) : [];
+    }
+    return [];
   });
 
   useEffect(() => {
-    localStorage.setItem('get item', val);
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('get item', val);
+    }
   }, [val]);
 
   useEffect(() => {
-    localStorage.setItem('cart items', JSON.stringify(cartItems));
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('cart items', JSON.stringify(cartItems));
+    }
   }, [cartItems]);
 
   return (
